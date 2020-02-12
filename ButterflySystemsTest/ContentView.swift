17280c90a2ba367cf7229588438c7cc43030587a
Regan Russell <regan.russell@gmail.com>
2020-02-12 11:52:07 +1100

    Initial Commit

---
+++ ButterflySystemsTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButterflySystemsTest
+//
+//  Created by Regan Russell on 12/2/20.
+//  Copyright © 2020 Regan Russell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
